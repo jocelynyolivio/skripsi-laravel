c15400539977d5c3cab54b4f075c2c247c408b81
jocelynyolivio <106477710+jocelynyolivio@users.noreply.github.com>
2025-06-10 02:13:30 +0700

    favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ $title ?? 'SenyumQu Dental Clinic' }}</title>
+  <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
+
   <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{ asset('assets/css/bootstrap-icons.min.css') }}" rel="stylesheet">
 
